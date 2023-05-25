eb528a6f4bcfcf1c3088ad140314491a4ddeff36
MatiasRodriguez12 <matiasrodriguez552@gmail.com>
2023-05-25 13:26:30 -0300

    Se modifica cantidad de bits de operacion

--- mulPF/fuentes/mulPF_tb.vhd
+++ mulPF/fuentes/mulPF_tb.vhd
@@ -7,7 +7,7 @@ end;
 
 architecture mulPF_tb_arq of mulPF_tb is
 	
-	constant	Nb_tb		: natural:= 32;
+	constant	Nb_tb		: natural:= 33;
 	constant	Nb_exp_tb	: natural:= 8;
 	constant	Nb_frac_tb	: natural:= Nb_tb-Nb_exp_tb-1;
 	constant	Sesgo_tb	: natural:= 2**(Nb_exp_tb-1)-1;
@@ -22,17 +22,31 @@ architecture mulPF_tb_arq of mulPF_tb is
 
 begin
 
+	-- Para 32 bits de longitud
+	--a_tb<= '0' & std_logic_vector(to_unsigned(Sesgo_tb+a_exp_tb,Nb_exp_tb)) & std_logic_vector(to_unsigned(0,Nb_frac_tb)), 
+	--		X"3FC00000" after 100 ns,
+	--		X"40DA0000" after 300 ns,
+	--		X"BFC00000" after 500 ns,
+	--		X"C0DA0000" after 700 ns;
+
+	--b_tb<= '0' & std_logic_vector(to_unsigned(Sesgo_tb+b_exp_tb,Nb_exp_tb)) & std_logic_vector(to_unsigned(0,Nb_frac_tb)), 
+	--		X"41A18000" after 200 ns,
+	--		X"BEB00000" after 400 ns,
+	--		X"3F500000" after 600 ns,
+	--		X"C1A18000" after 800 ns;
+
+	-- Para 33 bits de longitud
 	a_tb<= '0' & std_logic_vector(to_unsigned(Sesgo_tb+a_exp_tb,Nb_exp_tb)) & std_logic_vector(to_unsigned(0,Nb_frac_tb)), 
-			X"3FC00000" after 100 ns,
-			X"40DA0000" after 300 ns,
-			X"BFC00000" after 500 ns,
-			X"C0DA0000" after 700 ns;
+			'0'&X"7F800000" after 100 ns,
+			'0'&X"81B40000" after 300 ns,
+			'1'&X"7F800000" after 500 ns,
+			'1'&X"81B40000" after 700 ns;
 
 	b_tb<= '0' & std_logic_vector(to_unsigned(Sesgo_tb+b_exp_tb,Nb_exp_tb)) & std_logic_vector(to_unsigned(0,Nb_frac_tb)), 
-			X"41A18000" after 200 ns,
-			X"BEB00000" after 400 ns,
-			X"3F500000" after 600 ns,
-			X"C1A18000" after 800 ns;
+			'0'&X"83430000" after 200 ns,
+			'1'&X"7D600000" after 400 ns,
+			'0'&X"7EA00000" after 600 ns,
+			'1'&X"83430000" after 800 ns;
 
 	DUT: entity work.mulPF
 		generic map(
