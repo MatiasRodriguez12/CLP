ec98e21972c0165c617e8ac019b5558b25b08ca5
MatiasRodriguez12 <matiasrodriguez552@gmail.com>
2023-05-31 18:50:39 -0300

    Se aplica formato a los archivos fuente

--- mulPF/fuentes/mulPF.vhd
+++ mulPF/fuentes/mulPF.vhd
@@ -3,47 +3,51 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity mulPF is
-	generic(
-		Nb:			natural:= 32;
-		Nb_exp:		natural:= 8
+	generic (
+		Nb     : natural := 32;
+		Nb_exp : natural := 8
 	);
-	port(
-		a_i: 	in std_logic_vector(Nb-1 downto 0);
-		b_i:	in std_logic_vector(Nb-1 downto 0);
-		mul_o: 	out std_logic_vector(Nb-1 downto 0)
+	port (
+		a_i   : in std_logic_vector(Nb - 1 downto 0);
+		b_i   : in std_logic_vector(Nb - 1 downto 0);
+		mul_o : out std_logic_vector(Nb - 1 downto 0)
 	);
 end;
 
 architecture mulPF_arq of mulPF is
-	constant Nb_frac							: natural:= Nb-Nb_exp-1;
-	constant Sesgo								: natural:= 2**(Nb_exp-1)-1;
 
-	signal a_exp,b_exp,mul_exp					: std_logic_vector(Nb_exp - 1 downto 0);
-	signal a_frac,b_frac						: std_logic_vector(Nb_frac downto 0);
-	signal a_sig,b_sig,mul_sig					: std_logic;
+	constant Nb_frac             : natural := Nb - Nb_exp - 1;
+	constant Sesgo               : natural := 2 ** (Nb_exp - 1) - 1;
 
-	signal sum_to_sum,sum_out,sum_out_extra		: std_logic_vector(Nb_exp downto 0);
-	signal sesgo_to_sum,sum_extra				: std_logic_vector(Nb_exp-1 downto 0);
-	signal mul_out								: std_logic_vector(2*Nb_frac+1 downto 0);
-	signal mul_frac								: std_logic_vector(Nb_frac-1 downto 0);
+	signal a_exp, b_exp, mul_exp : std_logic_vector(Nb_exp - 1 downto 0);
+	signal a_mag, b_mag          : std_logic_vector(Nb_frac downto 0);
+	signal a_sig, b_sig, mul_sig : std_logic;
+
+	signal sum_to_sum, sum_out   : std_logic_vector(Nb_exp downto 0);
+	signal sesgo_to_sum          : std_logic_vector(Nb_exp - 1 downto 0);
+	signal mul_out               : std_logic_vector(2 * Nb_frac + 1 downto 0);
+	signal mul_red, mul_frac     : std_logic_vector(Nb_frac downto 0);
+	signal sum_red               : std_logic_vector(Nb_frac - 1 downto 0);
 
 begin
 
-	a_sig  <= a_i(Nb-1);
-	b_sig  <= b_i(Nb-1);
-	a_exp  <= a_i(Nb-2 downto Nb_frac);
-	b_exp  <= b_i(Nb-2 downto Nb_frac);
-	a_frac <= '1' & a_i(Nb_frac-1 downto 0);
-	b_frac <= '1' & b_i(Nb_frac-1 downto 0);
+	a_sig        <= a_i(Nb - 1);
+	b_sig        <= b_i(Nb - 1);
+	a_exp        <= a_i(Nb - 2 downto Nb_frac);
+	b_exp        <= b_i(Nb - 2 downto Nb_frac);
+	a_mag        <= '1' & a_i(Nb_frac - 1 downto 0);
+	b_mag        <= '1' & b_i(Nb_frac - 1 downto 0);
 
-	sesgo_to_sum <= std_logic_vector(to_unsigned(Sesgo,Nb_exp)) when mul_out(2*Nb_frac+1)='0' else std_logic_vector(to_unsigned(Sesgo-1,Nb_exp));
-	sum_extra	 <= std_logic_vector(to_unsigned(1,Nb_exp));
+	sesgo_to_sum <= std_logic_vector(to_unsigned(Sesgo, Nb_exp)) when mul_out(2 * Nb_frac + 1) = '0' else std_logic_vector(to_unsigned(Sesgo - 1, Nb_exp));
 
-	mul_exp	 <=	sum_out(Nb_exp-1 downto 0);
-	mul_frac <=	mul_out(2*Nb_frac-1 downto Nb_frac) when mul_out(2*Nb_frac+1)='0' else mul_out(2*Nb_frac downto Nb_frac+1);
-	mul_sig	 <= a_sig xor b_sig;
+	mul_red      <= mul_out(2 * Nb_frac - 1 downto Nb_frac - 1) when mul_out(2 * Nb_frac + 1) = '0' else mul_out(2 * Nb_frac downto Nb_frac);
+	sum_red      <= (Nb_frac - 2 downto 0 => '0') & mul_red(0);
 
-	sumNb_inst1: entity work.sumresNb
+	mul_sig      <= a_sig xor b_sig;
+	mul_exp      <= sum_out(Nb_exp - 1 downto 0);
+
+	-- Sumo los exponentes de ambas entradas
+	sum_exp_inst1 : entity work.sumresNb
 		generic map(
 			N => Nb_exp
 		)
@@ -51,32 +55,47 @@ begin
 			a_i  => a_exp,
 			b_i  => b_exp,
 			ci_i => '0',
-			s_o  => sum_to_sum(Nb_exp-1 downto 0),
+			s_o  => sum_to_sum(Nb_exp - 1 downto 0),
 			co_o => sum_to_sum(Nb_exp)
 		);
 
-	sumNb_inst2: entity work.sumresNb
+	-- Resto sesgo (SESGO si no es necesaria la alineación de coma, SESGO-1 si es necesaria la alineación de coma)
+	res_sesgo_inst2 : entity work.sumresNb
 		generic map(
 			N => Nb_exp
 		)
 		port map(
-			a_i  => sum_to_sum(Nb_exp-1 downto 0),
+			a_i  => sum_to_sum(Nb_exp - 1 downto 0),
 			b_i  => sesgo_to_sum,
 			ci_i => '1',
-			s_o  => sum_out(Nb_exp-1 downto 0),
+			s_o  => sum_out(Nb_exp - 1 downto 0),
 			co_o => sum_out(Nb_exp)
 		);
 
-	mulNb_inst: entity work.mulNb
-			generic map(
-				N => Nb_frac+1
-			)
-			port map(
-				a_i  => a_frac,
-				b_i  => b_frac,
-				mul_o => mul_out
-			);
+	-- Multiplico las magnitudes de ambas entradas (tratándolos como números enteros sin signo)
+	mul_magnitud_inst3 : entity work.mulNb
+		generic map(
+			N => Nb_frac + 1
+		)
+		port map(
+			a_i   => a_mag,
+			b_i   => b_mag,
+			mul_o => mul_out
+		);
 
-	mul_o <= mul_sig & mul_exp & mul_frac;
+	-- Realizo un redondeo de la parte fraccionaria de la magnitud
+	redondeo_inst4 : entity work.sumresNb
+		generic map(
+			N => Nb_frac
+		)
+		port map(
+			a_i  => mul_red(Nb_frac downto 1),
+			b_i  => sum_red,
+			ci_i => '0',
+			s_o  => mul_frac(Nb_frac - 1 downto 0),
+			co_o => mul_frac(Nb_frac)
+		);
+
+	mul_o <= mul_sig & mul_exp & mul_frac(Nb_frac - 1 downto 0);
 
 end;
--- mulPF/fuentes/mulPF_tb.vhd
+++ mulPF/fuentes/mulPF_tb.vhd
@@ -6,39 +6,37 @@ entity mulPF_tb is
 end;
 
 architecture mulPF_tb_arq of mulPF_tb is
-	
-	constant	Nb_tb		: natural:= 32;
-	constant	Nb_exp_tb	: natural:= 8;
-	constant	Nb_frac_tb	: natural:= Nb_tb-Nb_exp_tb-1;
-	constant	Sesgo_tb	: natural:= 2**(Nb_exp_tb-1)-1;
-	
+
+	constant Nb_tb      : natural := 32;
+	constant Nb_exp_tb  : natural := 8;
+	constant Nb_frac_tb : natural := Nb_tb - Nb_exp_tb - 1;
+	constant Sesgo_tb   : natural := 2 ** (Nb_exp_tb - 1) - 1;
+
 	-- Declaracion de senales de prueba
-	signal a_tb		: std_logic_vector(Nb_tb-1 downto 0);
-	signal b_tb		: std_logic_vector(Nb_tb-1 downto 0);
-	signal mul_tb	: std_logic_vector(Nb_tb-1 downto 0);
-	signal a_exp_tb	: natural:= 0;
-	signal b_exp_tb	: natural:= 0;
-	signal clk		: std_logic:='0';
-	
-
+	signal a_tb         : std_logic_vector(Nb_tb - 1 downto 0);
+	signal b_tb         : std_logic_vector(Nb_tb - 1 downto 0);
+	signal mul_tb       : std_logic_vector(Nb_tb - 1 downto 0);
+	signal a_exp_tb     : natural   := 0;
+	signal b_exp_tb     : natural   := 0;
+	signal clk          : std_logic := '0';
 begin
-	clk <= not clk after 100 ns;
+	clk  <= not clk after 100 ns;
 	-- Para 32 bits de longitud
-	a_tb<= '0' & std_logic_vector(to_unsigned(Sesgo_tb+a_exp_tb,Nb_exp_tb)) & std_logic_vector(to_unsigned(0,Nb_frac_tb)), 
-			X"3FC00000" after 100 ns,
-			X"40DA0000" after 300 ns,
-			X"BFC00000" after 500 ns,
-			X"C0DA0000" after 700 ns,
-			X"3FC2AB0C" after 900 ns;
+	a_tb <= '0' & std_logic_vector(to_unsigned(Sesgo_tb + a_exp_tb, Nb_exp_tb)) & std_logic_vector(to_unsigned(0, Nb_frac_tb)),
+		X"3FC00000" after 100 ns,
+		X"40DA0000" after 300 ns,
+		X"BFC00000" after 500 ns,
+		X"C0DA0000" after 700 ns,
+		X"3FC2AB0C" after 900 ns;
 
-	b_tb<= '0' & std_logic_vector(to_unsigned(Sesgo_tb+b_exp_tb,Nb_exp_tb)) & std_logic_vector(to_unsigned(0,Nb_frac_tb)), 
-			X"41A18000" after 200 ns,
-			X"BEB00000" after 400 ns,
-			X"3F500000" after 600 ns,
-			X"C1A18000" after 800 ns,
-			X"C1A1FAED" after 900 ns;
+	b_tb <= '0' & std_logic_vector(to_unsigned(Sesgo_tb + b_exp_tb, Nb_exp_tb)) & std_logic_vector(to_unsigned(0, Nb_frac_tb)),
+		X"41A18000" after 200 ns,
+		X"BEB00000" after 400 ns,
+		X"3F500000" after 600 ns,
+		X"C1A18000" after 800 ns,
+		X"C1A1FAED" after 900 ns;
 
-		-- Para 64 bits de longitud
+	-- Para 64 bits de longitud
 	-- a_tb<= '0' & std_logic_vector(to_unsigned(Sesgo_tb+a_exp_tb,Nb_exp_tb)) & std_logic_vector(to_unsigned(0,Nb_frac_tb)), 
 	-- 	X"3FF8000000000000" after 100 ns,
 	-- 	X"401B400000000000" after 300 ns,
@@ -51,15 +49,15 @@ begin
 	-- 	X"3FEA000000000000" after 600 ns,
 	-- 	X"C034300000000000" after 800 ns;
 
-	DUT: entity work.mulPF
+	DUT : entity work.mulPF
 		generic map(
-			Nb 		=> Nb_tb,		
-			Nb_exp 	=> Nb_exp_tb
+			Nb     => Nb_tb,
+			Nb_exp => Nb_exp_tb
 		)
 		port map(
-			a_i	 	=> a_tb, 
-			b_i	 	=> b_tb,
-			mul_o 	=> mul_tb
+			a_i   => a_tb,
+			b_i   => b_tb,
+			mul_o => mul_tb
 		);
-	
+
 end;
